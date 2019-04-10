4674f6847473c1fa73fedbd40bb4fdb332c5e654
Jahaziel <jahaziel.vaz@gmail.com>
2019-04-09 20:56:13 -0600

    Working on understanding the RegExp constructor

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const citiesArray = [];
+let userInput = document.getElementById('user-input').value;
 
 const citiesData = "https://gist.githubusercontent.com/Miserlou/c5cd8364bf9b2420bb29/raw/2bf258763cdddd704f8ffd3ea9a3e81d25e2c6f6/cities.json";
 
@@ -7,6 +8,10 @@ fetch(citiesData)
   .then(data => citiesArray.push(...data))
   .catch(error => console.error(error))
 
-citiesArray.filter(city => {
-  
-})
+
+  let findMatches = (wordToMatch, citiesArray) => {
+    return citiesArray.filter(place => {
+      const regex = new RegExp(wordToMatch, 'gi'); // This is called the RegExp constructor.
+      return place.city.match(regex) || place.state.match(regex)
+    })
+  }
